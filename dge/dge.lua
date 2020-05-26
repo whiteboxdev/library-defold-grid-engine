c7e51da547aab69924a5df6c168eaaffefa0fc08
Klayton Kowalski <gymratgames@gmail.com>
2020-05-25 23:49:17 -0500

    Added dge.reach().

--- dge/dge.lua
+++ dge/dge.lua
@@ -30,15 +30,27 @@ local dge = {}
 -- CONSTANTS
 ----------------------------------------------------------------------
 
+local ordinal_scaler = 1 / math.sqrt(2)
 
+local direction = {
+	up          = 1,
+	left        = 2,
+	down        = 4,
+	right       = 8,
+	up_left     = 3,
+	down_left   = 6,
+	down_right  = 12,
+	up_right    = 9
+}
 
 ----------------------------------------------------------------------
 -- MODULE PROPERTIES
 ----------------------------------------------------------------------
 
 dge.members = {}
-dge.debug = false
-dge.stride = 16
+dge.debug   = false
+dge.stride  = 0
+dge.ordinal = true
 
 ----------------------------------------------------------------------
 -- MODULE FUNCTIONS
@@ -47,6 +59,7 @@ dge.stride = 16
 function dge.init(config)
 	dge.debug = config.debug
 	dge.stride = config.stride
+	dge.ordinal = config.ordinal
 	if config.debug then
 		print("DGE: Initialized.")
 	end
@@ -60,17 +73,56 @@ function dge.register(config)
 	-- INSTANCE PROPERTIES
 	----------------------------------------------------------------------
 
-	local member = {}
-	local speed = config.speed
-	local input = { up = 0, left = 0, down = 0, right = 0 }
-	local moving = false
+	local member  = {}
+	local speed   = config.speed
+	local input   = { up = 0, left = 0, down = 0, right = 0 }
+	local moving  = false
 	local elapsed = 0
-	local start = go.get_position()
-	local target = start
+	local start   = go.get_position()
+	local target  = start
+	local facing  = direction.down
 	
 	----------------------------------------------------------------------
 	-- INSTANCE FUNCTIONS
 	----------------------------------------------------------------------
+
+	local function lerp(dt)
+		elapsed = elapsed + dt * speed
+		local progress = vmath.lerp(elapsed, start, target)
+		if elapsed >= 1 then
+			elapsed = 0
+			moving = false
+			start = target
+			progress = target
+		end
+		go.set_position(progress)
+	end
+
+	function member.reach()
+		local result = go.get_position()
+		if facing == direction.up then
+			result.y = result.y + dge.stride
+		elseif facing == direction.left then
+			result.x = result.x - dge.stride
+		elseif facing == direction.down then
+			result.y = result.y - dge.stride
+		elseif facing == direction.right then
+			result.x = result.x + dge.stride
+		elseif facing == direction.up_left then
+			result.x = result.x - dge.stride
+			result.y = result.y + dge.stride
+		elseif facing == direction.down_left then
+			result.x = result.x - dge.stride
+			result.y = result.y - dge.stride
+		elseif facing == direction.down_right then
+			result.x = result.x + dge.stride
+			result.y = result.y - dge.stride
+		elseif facing == direction.up_right then
+			result.x = result.x + dge.stride
+			result.y = result.y + dge.stride
+		end
+		return { x = math.floor(result.x / dge.stride + 1), y = math.floor(result.y / dge.stride + 1) }
+	end
 
 	function member.move_up()
 		input.up = 1
@@ -106,33 +158,31 @@ function dge.register(config)
 
 	function member.update(dt)
 		if moving then
-			elapsed = elapsed + dt
-			local ratio = dge.stride / speed
-			local progress = vmath.lerp(elapsed * ratio, start, target)
-			if elapsed >= 1 then
-				elapsed = 0
-				moving = false
-				start = target
-				progress = target
-			end
-			go.set_position(progress)
-		else
-			if input.up ~= 0 then
-				moving = true
-				target = target + vmath.vector3(0, dge.stride, 0)
-			end
-			if input.left ~= 0 then
-				moving = true
-				target = target + vmath.vector3(-dge.stride, 0, 0)
-			end
-			if input.down ~= 0 then
-				moving = true
-				target = target + vmath.vector3(0, -dge.stride, 0)
-			end
-			if input.right ~= 0 then
-				moving = true
-				target = target + vmath.vector3(dge.stride, 0, 0)
-			end
+			lerp(dt)
+			return
+		end
+		if input.up ~= 0 then
+			moving = true
+			target = target + vmath.vector3(0, dge.stride, 0)
+			facing = bit.bor(facing, direction.up)
+		end
+		if input.left ~= 0 then
+			moving = true
+			target = target + vmath.vector3(-dge.stride, 0, 0)
+			facing = bit.bor(facing, direction.left)
+		end
+		if input.down ~= 0 then
+			moving = true
+			target = target + vmath.vector3(0, -dge.stride, 0)
+			facing = bit.bor(facing, direction.down)
+		end
+		if input.right ~= 0 then
+			moving = true
+			target = target + vmath.vector3(dge.stride, 0, 0)
+			facing = bit.bor(facing, direction.right)
+		end
+		if moving then
+			lerp(dt)
 		end
 	end
 
--- example/player.go
+++ example/player.go
@@ -13,3 +13,23 @@ components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/example/atlas.atlas\"\n"
+  "default_animation: \"player_idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
